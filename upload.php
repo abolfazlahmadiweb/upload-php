4bc96b5df9519fcbaa2634bf7e7a22de6bd3234f
Abolfazl Ahmadi <aa@parsos.ir>
2022-10-23 12:32:54 -0700

    max 10 MB file size and redirect to index page after upload

--- upload.php
+++ upload.php
@@ -6,9 +6,14 @@ $ghesmat1 = explode('.', $file['name']);
 
 $ghesmat2 = end($ghesmat1);
 
-if(in_array($ghesmat2, ['png', 'jpeg', 'jpeg', 'mp3'])){
-    $ghesmat3 = 'img-'.rand(10, 5000).'.'.$ghesmat2;
-    move_uploaded_file($file['tmp_name'], 'images/'.$ghesmat3);
+if(end($file) < 10000000){
+    if(in_array($ghesmat2, ['png', 'jpeg', 'jpeg', 'mp3'])){
+        $ghesmat3 = 'img-'.rand(10, 5000).'.'.$ghesmat2;
+        move_uploaded_file($file['tmp_name'], 'images/'.$ghesmat3);
+        header('Location:index.php');
+    }else{
+        echo 'فرمت فایل مورد نظر پشتیبانی نمیشود';
+    }
 }else{
-    echo 'فرمت فایل مورد نظر پشتیبانی نمیشود';
+    echo 'حجم فایل بیشتر از 10 مگابایت است';
 }
